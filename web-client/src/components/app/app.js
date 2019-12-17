30171a62e706b507784ba39c8889e5404806c9f2
Ilya Makarevich <ilya_makarevich@epam.com>
2019-12-17 13:59:07 +0300

    add eslint

--- web-client/src/components/app/app.js
+++ web-client/src/components/app/app.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import './app.scss';
 
 export default class App extends Component {
+  buttonClick() {
+    console.log('test click');
+  }
 
-    buttonClick() {
-        console.log('test click');
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <h1>Hello world!</h1>
-                <button type='button' onClick={ this.buttonClick }>Test input click</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello world!</h1>
+        <button type="button" onClick={this.buttonClick}>Test input click</button>
+      </div>
+    );
+  }
 }
--- web-client/src/components/app/app.test.js
+++ web-client/src/components/app/app.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import App from '../app/app';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from './app';
 
-configure({adapter: new Adapter() });
- 
+configure({ adapter: new Adapter() });
+
 describe('App Component', () => {
-    it("check App input", () => {
-        const appComponent = shallow(<App />),
-            input = appComponent.find("button[type='button']");
-        input.simulate("click");
-    });
-  })
+  it('check App input', () => {
+    const appComponent = shallow(<App />);
+    const input = appComponent.find("button[type='button']");
+    input.simulate('click');
+  });
+});
