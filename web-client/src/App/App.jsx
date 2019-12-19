6b2b6b93a6b65fa11520d4831072e1877af4da95
Yauheni_Tulia@epam.com <yauheni_tulia@epam.com>
2019-12-19 16:41:34 +0300

    feature/cb-4 added paintedButton and NoPaintedButton

--- web-client/src/App/App.jsx
+++ web-client/src/App/App.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
-import Input from '../common/components/input';
 import PaintedButton from '../common/components/PaintedButton';
 import NoPaintedButton from '../common/components/NoPaintedButton';
-import Icon from '../common/components/icon';
-import Image from '../common/components/image';
-import Checkbox from '../common/components/checkbox';
 
 import './app.scss';
 
@@ -14,13 +10,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Input name="Email" label="Email" htmlFor="Email" title="Email" placeholder="email" />
-        <Input type="password" name="password" label="password" htmlFor="password" title="Password" />
         <PaintedButton className="blue-outline" caption="Register" />
         <NoPaintedButton className="base" caption="Login" />
-        <Icon />
-        <Image />
-        <Checkbox htmlFor="checkbox" name="checkbox" title="Remember me" htmlFor="checkbox" id="checkbox" />
       </div>
     );
   }
