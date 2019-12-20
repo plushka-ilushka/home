fbf00b4150c4e35c234c10089b8a811e0b219799
Ilya Makarevich <ilya_makarevich@epam.com>
2019-12-20 10:55:35 +0300

    create inputs

--- web-client/src/App/App.jsx
+++ web-client/src/App/App.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 
 import Input from '../common/components/input';
-import PaintedButton from '../common/components/PaintedButton';
-import NoPaintedButton from '../common/components/NoPaintedButton';
-import Icon from '../common/components/icon';
-import Image from '../common/components/image';
-import Checkbox from '../common/components/checkbox';
-
-import shape from '../assets/icons/shape.svg';
-import key from '../assets/icons/key.svg';
-import suche from '../assets/icons/suche.svg';
 
 import './app.scss';
 
@@ -20,13 +11,6 @@ export default class App extends Component {
       <div>
         <Input name="Email" label="Email" htmlFor="Email" title="Email" placeholder="email" />
         <Input type="password" name="password" label="password" htmlFor="password" title="Password" />
-        <PaintedButton caption="Register" />
-        <NoPaintedButton caption="Login" />
-        <Icon src={shape} />
-        <Icon src={key} />
-        <Icon src={suche} />
-        <Image />
-        <Checkbox htmlFor="checkbox" name="checkbox" title="Remember me" htmlFor="checkbox" id="checkbox" />
       </div>
     );
   }
