fc51f4911ee90a73db146b7bbddcfc43b1a865e4
Ilya Makarevich <ilya_makarevich@epam.com>
2019-12-24 12:45:23 +0300

    feature/cb-8 add constants

--- web-client/src/common/components/input/Input.jsx
+++ web-client/src/common/components/input/Input.jsx
@@ -2,50 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import INPUT_TYPES from './constants';
+
 import './input.scss';
 
+
 export default function Input(props) {
+  const {
+    className,
+    name,
+    type,
+    value,
+    onChange,
+    placeholder,
+    ...restProps
+  } = props;
 
-    const {
-        title,
-        className,
-        name,
-        type,
-        value,
-        handleChange,
-        placeholder,
-        ...restProps
-    } = props;
-
-    return (
-            <input
-                className={classNames("input", className)}
-                id={name}
-                name={name}
-                type={type}
-                value={value}
-                onChange={handleChange}
-                placeholder={placeholder}
-                {...restProps}
-            />
-    );
-};
+  return (
+    <input
+      className={classNames('input', className)}
+      id={name}
+      name={name}
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      {...restProps}
+    />
+  );
+}
 
 Input.propTypes = {
-    className: PropTypes.string,
-    id: PropTypes.string,
-    name: PropTypes.string,
-    type: PropTypes.string,
-    value: PropTypes.string,
-    onChange: PropTypes.bool,
-    placeholder: PropTypes.string
+  className: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
 };
 
 Input.defaultProps = {
-    type: "text",
-    className: undefined,
-    name: "",
-    type: undefined,
-    placeholder: ""
+  type: INPUT_TYPES.TEXT,
+  className: undefined,
+  name: undefined,
+  placeholder: undefined,
+  id: undefined,
+  value: undefined,
+  onChange: undefined,
 };
-
--- web-client/src/common/components/label/Label.jsx
+++ web-client/src/common/components/label/Label.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-import "./label.scss";
+import './label.scss';
 
 export default function Label(props) {
-    const {
-        className,
-        children,
-    } = props;
+  const {
+    className,
+    children,
+  } = props;
 
-    return (
-        <label className = {classNames('label', className)}>
-            {children}
-        </label>
-    )
+  return (
+    <label className={classNames('label', className)}>
+      {children}
+    </label>
+  );
 }
+
+Label.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Label.defaultProps = {
+  className: undefined,
+};
