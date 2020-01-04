be49777479d5913e7407908239b35ee09210db14
Uladzislau Svintsitsitski <uladzislau_svintsitski@epam.com>
2020-01-04 07:34:07 +0300

    remove tests for Input component

--- web-client/src/common/components/input/input.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import Input from './Input';
-
-describe('Input Component', () => {
-  it('Input Snaphot', () => {
-    expect(mount(<Input />)).toMatchSnapshot();
-  });
-});
